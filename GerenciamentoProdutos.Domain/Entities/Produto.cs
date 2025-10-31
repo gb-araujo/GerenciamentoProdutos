3f97ff1ec139abaf2fc44c7239773f5e43ab66f7
Gabriel Araujo <86437334+gb-araujo@users.noreply.github.com>
2025-10-31 11:52:12 -0300

    Adicionar arquivos de projeto.

---
+++ GerenciamentoProdutos.Domain/Entities/Produto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GerenciamentoProdutos.Domain.Entities;
+public class Produto
+{
+    public int Id { get; set; }
+    public string Nome { get; set; }
+    public string Descricao { get; set; }
+    public double Preco { get; set; }
+    public string Categoria { get; set; }
+}
